0e9841498ff46177c47ff5b9c81384f8933b0abd
Zhen Li <zl694@cornell.edu>
2019-05-11 15:46:37 +0800

    bag manager

--- mwf/bag_util/bag_manager.py
+++ mwf/bag_util/bag_manager.py
@@ -1,4 +1,22 @@
 # TODO: BagManager
 
+from .bag import Bag
+
 class BagManager:
-    pass
+    _bag_dict = {}
+    _max_bag_id = 0
+
+    def add_bag(self,
+                bag_id: int = _max_bag_id + 1):
+
+        if bag_id in self._bag_dict:
+            raise ValueError(f"bag id {bag_id} exists")
+        self._bag_dict[bag_id] = Bag(bag_id=bag_id)
+        self._max_bag_id = max(self._max_bag_id, bag_id)
+
+    def get_bag_by_id(self, bag_id: int):
+        if bag_id not in self._bag_dict:
+            raise ValueError(f"npc id {bag_id} not found")
+        return self._bag_dict[bag_id]
+
+
